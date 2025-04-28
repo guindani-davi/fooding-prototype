99acc6cf4773f20267855598ccb8c810820eeed0
Davi-Guindani <davigpvieira@gmail.com>
2025-04-28 14:29:38 -0300

    add animal, player and scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float xRange = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+
+    }
+}
